2fe66410b041cc32edf4a8a102c63547946acb27
anonymousacc292 <gypsophila661@gmail.com>
2024-11-22 06:42:29 -0800

    fast run

--- fast_run.py
+++ fast_run.py
@@ -19,5 +19,18 @@ RUSTFLAGS="-Awarnings" cargo bench --bench t_out_of_n_sign
 print(run)
 subprocess.call(["bash", "-c", run])
 
+run = '''
+cd crypto
+RUSTFLAGS="-Awarnings" cargo test --release --package bbs_plus --lib -- threshold::threshold_bbs_plus::tests::signing_n_out_of_n --exact --show-output
+'''
+print(run)
+subprocess.call(["bash", "-c", run])
+
+run = '''
+RUSTFLAGS="-Awarnings" cargo test --release --package bbs_plus --lib -- threshold::threshold_bbs_plus::tests::signing_t_out_of_n --exact --show-output
+'''
+print(run)
+subprocess.call(["bash", "-c", run])
+
 
 
